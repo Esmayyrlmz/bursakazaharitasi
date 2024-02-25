78e2ebbdc6da74ece7531292ea5cc65556ee6103
esmayyorulmaz9@gmail.com <esmayyorulmaz9@gmail.com>
2024-02-25 22:19:58 +0300

    dashboard delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Login from "./pages/login.jsx";
 import {Route, Routes} from "react-router-dom";
 import Register from "./pages/register.jsx";
-import Dashboard from "./pages/dashboard.jsx";
 import Home from "./pages/home.jsx";
 
 export default function App() {
@@ -10,7 +9,6 @@ export default function App() {
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/register' element={<Register/>}/>
-            <Route path='/dashboard' element={<Dashboard/>}/>
             <Route path='/*' element={<p>404</p>}/>
         </Routes>
     )
